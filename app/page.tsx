b757ccb08e00622e2c466474c8799df232c34323
Rahulbalakrishnan <newtoncorpse@gmail.com>
2024-02-10 18:21:56 +0530

    Basic setup

---
+++ app/page.tsx
@@ -0,0 +1,55 @@
+
+
+export default function Home() {
+  return (
+    <main className=" min-h-screen flex flex-col">
+
+
+
+      {/* TODO : Hero Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+
+
+
+
+      {/* TODO : Other Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+
+      {/* TODO : Other Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+
+      {/* TODO : Other Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+
+      {/* TODO : Other Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+
+      {/* TODO : Other Page */}
+      <div className="w-full h-screen ">
+        <section>
+          <h1>Seed nano tech...</h1>
+        </section>
+      </div>
+    </main>
+  );
+}
